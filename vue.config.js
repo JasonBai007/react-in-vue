79b53c4d2ab6f0e31a86417a1fd3450a48d70d9e
JasonBai007 <byygkcg@163.com>
2022-10-27 15:46:28 +0800

    重新发布

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "react-in-vue",
+  publicPath: "/react-in-vue/",
   outputDir: "docs",
   devServer: {
     host: "127.0.0.1",
