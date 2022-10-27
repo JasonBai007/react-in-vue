9de550e34ba4516cec7bf2eeadae9323e2c42afa
JasonBai007 <byygkcg@163.com>
2022-10-27 15:43:20 +0800

    修改发布路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "react-in-vue",
   outputDir: "docs",
   devServer: {
     host: "127.0.0.1",
